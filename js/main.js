5cf0fb56a84d1e0a8d61469c45a31fb14a4ddd47
Callum Lu <610721230@gms.ndhu.edu.tw>
2020-05-07 17:36:21 +0800

    Site updated: 2020-05-07 17:36:20

--- js/main.js
+++ js/main.js
@@ -1,59 +1,8 @@
 $(document).ready( function () {
-    let imgInit = function () {
-        $('article .container img').each(function () {
-            let imgPath = $(this).attr('src');
-            $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
-
-            $(this).addClass("img-shadow img-margin");
-            let captionText = $(this).attr('alt') || $(this).attr('title') || "";
-            
-            if (captionText !== "") {
-                let caption = "<div class='img-caption d-block text-center'>";
-                caption += "<span class='center-caption text-muted border-bottom'>" + captionText + "</span>";
-                caption += "</div>";
-
-                let captionHtml = $.parseHTML(caption);
-                $(this).parent().append(captionHtml);
-            }
-        });
-    };
-    imgInit();
-
     $('article').lightGallery({
         selector: '.img-item',
         subHtmlSelectorRelative: true
     });
-
-    let tableInit = function () {
-        $('article .container table').each(function () {
-            if (!($(this).parent().hasClass("highlight"))) {
-                $(this).wrap('<div class="table-responsive"></div>');
-                $(this).addClass('table table-bordered');
-            }
-        });
-    }
-    tableInit();
-
-    let highlightInit = function () {
-        $('article .container figure:has(td.gutter)').each(function () {
-            $(this).addClass('line_number');
-        });
-    }
-    highlightInit();
-
-    let footnoteInit = function () {
-        $("#footnotes hr").addClass("block-hr");
-        $("#footnotelist").addClass("card card-default");
-        $("#footnotelist ol").addClass("list-group list-group-flush");
-        $("#footnotelist li").addClass("list-group-item");
-        $("#footnotelist ol").css({"margin-left": "0"});
-        $("#footnotelist li span:first-child").css({"margin-left": "0"});
-
-        var heading = '<div class="card-header">Reference</div>'
-        let headingHtml = $.parseHTML(heading);
-        $("#footnotelist").prepend(headingHtml);
-    }
-    footnoteInit();
 
     let tocInit = function () {
         $("#toc-content a").each( function () {
@@ -70,12 +19,12 @@ $(document).ready( function () {
 
     let cardListInit = function () {  
 	    $(".card ul").each( function () {
-	if ($(this).parent().hasClass("card-body")) {
-		$(this).unwrap();
-	}
-	    });
-	$(".card ul").addClass("list-group list-group-flush");
-	$(".card ul li").addClass("list-group-item");
+            if ($(this).parent().hasClass("card-body")) {
+                $(this).unwrap();
+            }
+        });
+        $(".card ul").addClass("list-group list-group-flush");
+        $(".card ul li").addClass("list-group-item");
     }
     cardListInit();
 
@@ -91,19 +40,19 @@ $(document).ready( function () {
 
     $('.expand-toggle').on('click', function () {
         $("#toc-content").toggleClass("expand");
-        $(".expand-text").toggle()
-        $(".close-text").toggle()
+        $(".expand-text").toggle();
+        $(".close-text").toggle();
     })
 
     $('.back-to-top').on('click', function () {
         $('body, html').stop(true, true).animate({
             scrollTop: 0
-        }, 100, 'linear')
+        }, 100, 'linear');
     });
 
     $('.go-to-bottom').on('click', function () {
         $('body, html').stop(true, true).animate({
             scrollTop: $(document.body)[0].scrollHeight
-        }, 100, 'linear')
+        }, 100, 'linear');
     });
 });
